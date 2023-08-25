6f02c1b725e7f38d4d624bd2c4659f6231fad141
Musili Brian <musilibrian07@gmail.com>
2023-08-25 06:19:46 +0300

    All operators complete

--- src/main/java/com/java_se_training/operators/OperatorsDemo.java
+++ src/main/java/com/java_se_training/operators/OperatorsDemo.java
@@ -7,6 +7,7 @@ public class OperatorsDemo {
      private static final Logger LOGGER = Logger.getLogger(OperatorsDemo.class.getName()); 
 
      public void arithmeticOperators (){
+
         //addition
         int math = 23;
         int eng = 25;
@@ -31,8 +32,60 @@ public class OperatorsDemo {
         LOGGER.info(("modulus of the marks " + modulaMarks));
      }
 
-    public static void main(String[] args) {
+     public void assignmentOperators (){
+        int quantity = 45;
+
+        //additional
+        // quantity= quantity + 10;
+        quantity += 10;
+
+        LOGGER.info("quantity: " + quantity);
+
+        //multiply
+         quantity *= 10;
+
+         LOGGER.info("quantity: " + quantity);
+
+         //equivalent
+         quantity /= 10;
+
+         LOGGER.info("quantity: " + quantity);
+     }
+    
+     public void comparisonOperators(){
+        int eng = 40;
+        int math = 40;
+
+        //equal
+        boolean isEqual = eng == math;
+        LOGGER.info("Comparison " + isEqual);
+
+        //not equal
+          boolean notEqual = eng != math;
+        LOGGER.info("Comparison not equal " + notEqual);
+
+        //greater
+          boolean isCheckingGreater = eng > math;
+        LOGGER.info("Comparison greater " + isCheckingGreater);
+
+        //greater or equal
+           boolean isCheckingGreaterOrEqual = eng >= math;
+        LOGGER.info("Comparison greater or equal " + isCheckingGreaterOrEqual);
+
+
+        //less
+          boolean isCheckingLess = eng < math;
+        LOGGER.info("Comparison lesser " + isCheckingLess);
+
+        //greater or less
+           boolean isCheckingLessOrEqual = eng <= math;
+        LOGGER.info("Comparison greater or equal " + isCheckingLessOrEqual);
+     }
+    
+     public static void main(String[] args) {
         OperatorsDemo app = new OperatorsDemo();
-        app.arithmeticOperators();
+        // app.arithmeticOperators();
+        //app.assignmentOperators();
+        app.comparisonOperators();
     }
 }

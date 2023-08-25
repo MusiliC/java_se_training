64ba37192e15f893c127e004169267f704bead8a
Musili Brian <musilibrian07@gmail.com>
2023-08-25 05:22:21 +0300

    Wrapper classes for primitives

--- src/main/java/com/java_se_training/variables/variablesDemo.java
+++ src/main/java/com/java_se_training/variables/variablesDemo.java
@@ -7,7 +7,7 @@ public class variablesDemo {
     private static final Logger LOGGER = Logger.getLogger(variablesDemo.class.getName()); 
 
     private void constantVariables () {
-              //constant variable //constants - upper case letters
+         //constant variable //constants - upper case letters
         // ? final keyword -> type -> variable name -> = -> value -> ;
         final double PI = 3.1242;
         LOGGER.info("Constant variable PI is: " + PI);
@@ -17,16 +17,42 @@ public class variablesDemo {
           int score = 0;
         LOGGER.info("Score value is: " + score); 
         LOGGER.info("Maximum value for integer: " + Integer.MAX_VALUE );
+
+        byte aPrimitive = 23;
+        // ? Wrapper classes comes with additional methods eg intValue() 
+        Byte aWrapper = 23; 
+        int  a = aWrapper.intValue();
+        LOGGER.info("Get int value fro byte wrapper " + a);
+
+        //short
+        short s = 23;
+        Short sWrapper = 23;
+
+        //int
+        int i = 23;
+        Integer iWrapper = 23;
+
+        //long
+        long l = 23;
+        Long lWrapper = 23L;
+
+        
     }
     private void floatingTypes(){
         float floatNumber = 1.22F;
         LOGGER.info("Floating value: " + floatNumber);
+
+         float f = 23F;
+        Float fWrapper = 23F;
         
     }
     private void charTypes(){
         char grade = 'A';
         LOGGER.info("Your grade is: " + grade);
-        
+
+        Character cWrapper = 'A';
+
+               
     }
     private void booleanTypes(){
         boolean isSubmitted = true;

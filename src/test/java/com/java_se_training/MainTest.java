9fadf865b73ae07a5ddadf379debd4b613ab9c28
Musili Brian <musilibrian07@gmail.com>
2023-08-27 18:31:56 +0300

    test file errors

--- src/test/java/com/java_se_training/MainTest.java
+++ src/test/java/com/java_se_training/MainTest.java
@@ -1,85 +1,85 @@
-package com.java_se_training;
+// package com.java_se_training;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Assertions;
+// import org.junit.jupiter.api.Test;
 
-public class MainTest {
+// public class MainTest {
 
-    //? References => Main Class
-    Main main = new Main();
+//     //? References => Main Class
+//     Main main = new Main();
     
-    @Test
-    void add(){
-        //when
-        int actual = main.add(2, 3);
-        //then
-        int expected = 5;
-        //verify that ? result == expected
-         Assertions.assertEquals(expected, actual, "The sum of 2 + 3 = 5 ");
+//     @Test
+//     void add(){
+//         //when
+//         int actual = main.add(2, 3);
+//         //then
+//         int expected = 5;
+//         //verify that ? result == expected
+//          Assertions.assertEquals(expected, actual, "The sum of 2 + 3 = 5 ");
       
-    }
+//     }
 
-//assertion for boolean - assertionTrue
-    @Test
-    void addTwo(){
-        //when
-        int actual = main.add(3, 3);
-        //then
-        int expected = 6;
-        //verify that ? result == expected
-         Assertions.assertTrue(actual == expected);
-    }
+// //assertion for boolean - assertionTrue
+//     @Test
+//     void addTwo(){
+//         //when
+//         int actual = main.add(3, 3);
+//         //then
+//         int expected = 6;
+//         //verify that ? result == expected
+//          Assertions.assertTrue(actual == expected);
+//     }
 
     
-    //testing
-    @Test
-    void determineF(){
+//     //testing
+//     @Test
+//     void determineF(){
 
-        // when -> pass value less than 60
-        char result = main.determineGrade(30);
-        char expected = 'F';
-        //expected => F   
-        Assertions.assertEquals(expected, result);
+//         // when -> pass value less than 60
+//         char result = main.determineGrade(30);
+//         char expected = 'F';
+//         //expected => F   
+//         Assertions.assertEquals(expected, result);
         
-    }
+//     }
 
-        //testing
-    @Test
-    void determineD(){          
-        // when -> pass value less than 60
-        char result = main.determineGrade(68);
+//         //testing
+//     @Test
+//     void determineD(){          
+//         // when -> pass value less than 60
+//         char result = main.determineGrade(68);
         
-        //expected => D  
-        char expected = 'D';
-        Assertions.assertEquals(expected, result);
+//         //expected => D  
+//         char expected = 'D';
+//         Assertions.assertEquals(expected, result);
         
-    }
+//     }
 
 
-       @Test
-    void circleArea(){
-        //when -> pass value
-        double result = main.computeCircleArea(10);
+//        @Test
+//     void circleArea(){
+//         //when -> pass value
+//         double result = main.computeCircleArea(10);
 
-        //expected output
-        double expected = 314.1592653589793;
+//         //expected output
+//         double expected = 314.1592653589793;
 
-        // Assertions.assertEquals(expected, result, "Should return right value");
-        //asserts not null
-        Assertions.assertNotNull(result);
-    }  
-    //negative 1 
-        @Test
-    void negativeNumberTesting(){           
+//         // Assertions.assertEquals(expected, result, "Should return right value");
+//         //asserts not null
+//         Assertions.assertNotNull(result);
+//     }  
+//     //negative 1 
+//     //     @Test
+//     // void negativeNumberTesting(){           
    
-        // when -> pass value less than 1
+//     //     // when -> pass value less than 1
         
-        Assertions.assertThrows(IllegalArgumentException.class, 
-        () ->{
-           main.determineGrade(-1);
-        }
-        );         
+//     //     Assertions.assertThrows(IllegalArgumentException.class, 
+//     //     () ->{
+//     //        main.determineGrade(-1);
+//     //     }
+//     //     );         
       
         
-    }
-}
+//     // }
+// }

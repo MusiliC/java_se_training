aff311a1498b38a62e920a2e34c4dd6ca85500ec
Musili Brian <musilibrian07@gmail.com>
2023-08-31 05:42:01 +0300

    atm while do for showing menu until user quits

--- src/main/java/com/java_se_training/atmSimulator/AtmServices.java
+++ src/main/java/com/java_se_training/atmSimulator/AtmServices.java
@@ -11,6 +11,7 @@ public class AtmServices {
         final String password = "Admin123";
         boolean userSignedIn = false;
         double balance = 1000.00;
+    
         
         Scanner sn= new Scanner(System.in);  
         
@@ -27,12 +28,23 @@ public class AtmServices {
 
             if(userPassword.equals(password)){
                 userSignedIn = true;
+
+                while(true){                
                 
                 System.out.println("*********************\n");
                 System.out.println("ATM SIMULATOR\n");
                 System.out.println("*********************\n");
                 System.out.println("ATM SERVICES\n");
                 System.out.println("______________________\n");
+
+                   try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+               
+
                 System.out.println("1. Check Balance");
                 System.out.println("2. Deposit");
                 System.out.println("3. Withdrawal");
@@ -67,23 +79,27 @@ public class AtmServices {
                        
                         break;
 
+                        case 4:
+                              System.out.println("Service not available at the moment! ");
+                              break; 
+
                       case 5:
                           System.out.println("Thank you, Hope to see you later!"); 
                           sn.close(); // Close the scanner
                           return; // Exit the program                                                     
                 
                     default:
-                    System.out.println("Kindly choose a number between 1 - 5 with exemption of 4, thankyou");
-                        break;
+                    System.out.println("Kindly choose a number between 1 - 5 , thankyou");
+                    break;
                 }
-
+            } 
             }else {
                 System.out.println("Invalid password, Try again!");
             }
         }
 
         if(!userSignedIn) {
-            System.out.println("Exiting program.Try next time");
+            System.out.println("Exiting program.Try next time\n");
         } else{
             System.out.println("Always feel welcomed again, Thank you!");
         }

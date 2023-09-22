be24a05936479f4c3f8c70549743e83742652586
Musili Brian <musilibrian07@gmail.com>
2023-09-22 09:51:01 +0300

    abstract classes

---
+++ src/main/java/com/systechafrica/part2/abstractclasses/AbstractClassDemo.java
@@ -0,0 +1,10 @@
+package com.systechafrica.part2.abstractclasses;
+
+public class AbstractClassDemo {
+    public static void main(String[] args) {
+        Animal dog = new Dog("Malkia");
+
+        dog.eat();
+        dog.makeSound();
+    }
+}
